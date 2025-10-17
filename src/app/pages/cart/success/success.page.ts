51825088226a74650a7ea02c7369f3bda7a5bf21
Martin Martinov <m.martinov@kipo.bg>
2025-10-17 17:36:57 +0300

    clear cart v2

--- src/app/pages/cart/success/success.page.ts
+++ src/app/pages/cart/success/success.page.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SharedModule } from 'src/app/shared.module';
+import { CartStore } from '../store/cart.store';
 
 @Component({
   selector: 'app-success',
   templateUrl: './success.page.html',
   styleUrls: ['./success.page.scss'],
   standalone: true,
-  imports: [SharedModule]
+  imports: [SharedModule],
 })
 export class SuccessPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  readonly cartStore = inject(CartStore);
+  constructor() {
+    this.cartStore.clearCart();
   }
 
+  ngOnInit() {}
 }
